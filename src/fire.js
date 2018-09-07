9edfefbfcfaa883eb011deba914f5d383435e0e0
Alexey Khisamutdinov <alexey-khisamutdinov@users.noreply.github.com>
2018-09-06 21:53:30 -0700

    backup create-react readme file

--- src/fire.js
+++ src/fire.js
@@ -1,14 +1,7 @@
 // import firebase from 'firebase'
 import * as firebase from 'firebase/app';
+import {firebaseConfig} from 'secrets';
 import 'firebase/database';
 
-var config = {
-    "apiKey": "AIzaSyAJ_UFfGV8luR8M5UOekf0X-d9rUaMRUDg",
-    "databaseURL": "https://not-recommend.firebaseio.com",
-    "storageBucket": "not-recommend.appspot.com",
-    "authDomain": "not-recommend.firebaseapp.com",
-    "messagingSenderId": "57894742554",
-    "projectId": "not-recommend"
-};
-var fire = firebase.initializeApp(config);
+var fire = firebase.initializeApp(firebaseConfig);
 export default fire;
