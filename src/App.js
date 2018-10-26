0cae2246e5c3280eefb3145ca634fb7a356cd0ab
Yulia Khisamutdinova <y.khisamutdinova@gmail.com>
2018-10-26 09:29:16 -0700

    create react component for home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import fire from './fire'
+//import fire from './fire'
 
 class App extends Component {
   constructor(props) {
@@ -9,42 +9,51 @@ class App extends Component {
     this.state = { messages: [] }; // <- set up react state
   }
   componentWillMount(){
-    /* Create reference to messages in Firebase Database */
+    /* Create reference to messages in Firebase Database 
     let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
     messagesRef.on('child_added', snapshot => {
-      /* Update React state when message is added at Firebase Database */
+      /* Update React state when message is added at Firebase Database 
       let message = { text: snapshot.val(), id: snapshot.key };
       this.setState({ messages: [message].concat(this.state.messages) });
-    })
+    })*/
   }
   addMessage(e){
     e.preventDefault(); // <- prevent form submit from reloading the page
     /* Send the message to Firebase */
-    fire.database().ref('messages').push( this.inputEl.value );
-    this.inputEl.value = ''; // <- clear the input
+    //fire.database().ref('messages').push( this.inputEl.value );
+    //this.inputEl.value = ''; // <- clear the input
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to `Not Recommended App`</h1>
-          <h2 className="App-title">With Blackjack and Firebase</h2>
-        </header>
-        <div className="App-intro">
-          <form onSubmit={this.addMessage.bind(this)}>
-            <input type="text" ref={ el => this.inputEl = el }/>
-            <input type="submit"/>
-          </form>
+      <div id="body">
+        <div className="page-content">
+            <header>
+                <nav>
+                    <div id="review-button">
+                        <a href="#" className="button-style">Write a Review</a>
+                    </div>
+                    <div id="login-button">
+                        <a href="#" className="button-style">Sign Up</a>
+                        <a href="#" className="button-style">Log In</a>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <h1 className="logo"><img src={"img/drop.png"}/><span>CLEAR</span></h1>
+                <form action="/">
+                    <div>
+                    <label for="findinput">Find a buisness:</label>
+                    <input type="text" id="findinput" name="find" placeholder="What"/>
+                    <input type="text" name="location" placeholder="Where"/>
+                    <input type="submit" value="Search"/>
+                    </div>
+                </form>
+                <article>Content after Search button was activated</article> 
+            </main>
         </div>
-        <div className="App-intro">
-            <ul>
-                { /* Render the list of messages */
-                  this.state.messages.map( message => <li key={message.id}>{message.text}</li> )
-                }
-            </ul>
-        </div>
+            <footer><p>This site was created by Yulia Khisamutdinova and Alexey Khisamutdinov</p></footer>  
       </div>
+      
     );
   }
 }
--- src/fire.js
+++ src/fire.js
@@ -1,7 +1,7 @@
 // import firebase from 'firebase'
-import * as firebase from 'firebase/app';
+/*import * as firebase from 'firebase/app';
 import {firebaseConfig} from 'secrets';
 import 'firebase/database';
 
 var fire = firebase.initializeApp(firebaseConfig);
-export default fire;
+export default fire;*/
